94a17f109466a5c75c686fa56b54cefc7a21fe9b
Brandon Quintero <developerbvth@gmail.com>
2023-03-16 00:38:22 -0500

    datos de listas modificado

--- js/scrt__index.js
+++ js/scrt__index.js
@@ -1,6 +1,11 @@
 const lista1 = document.getElementById('lista1')
 const lista2 = document.getElementById('lista2')
 const lista3 = document.getElementById('lista3')
+const lista4 = document.getElementById('lista4')
+const lista5 = document.getElementById('lista5')
+const lista6 = document.getElementById('lista6')
+const lista7 = document.getElementById('lista7')
+const lista8 = document.getElementById('lista8')
 const btn = document.getElementById('btn')
 
 
